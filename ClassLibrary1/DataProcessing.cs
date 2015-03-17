3579c65e768b31c3ecf24683350bba653ccf62b2
fanatt20 <fanatt20@gmail.com>
2015-03-17 11:22:52 +0200

    Added Event in IProcessInfoHandler<T> and ProcessInfoHahdler<T>

--- ClassLibrary1/DataProcessing.cs
+++ ClassLibrary1/DataProcessing.cs
@@ -6,12 +6,15 @@ using System.Threading;
 
 namespace ClassLibrary1
 {
-    class ProcessInfoHandler<T> : IProcessInfoHandler<T> where T : IProcessInfoCategory,new()
+    class ProcessInfoHandler<T> : IProcessInfoHandler<T> where T : IProcessInfoCategory, new()
     {
+        public delegate void ProcessInfoEventHandler(Object sender, ProcessInfoEventArgs e);
+        public event ProcessInfoEventHandler StorageHasUpdated;
         Timer timer;
         IProcessStorage<T> coll;
         IProcessInfoGenerator gen = null;
         int interval;
+
         public void StartTrack(IProcessInfoGenerator generator, IProcessStorage<T> collection, int intervals)
         {
             coll = collection;
@@ -20,6 +23,7 @@ namespace ClassLibrary1
             timer = new Timer(new TimerCallback(Track), null, new TimeSpan(0, 0, 0), new TimeSpan(0, 0, intervals));
 
         }
+
         private void Track(object state)
         {
             string nameProcess = gen.GetCurrentProcessName();
@@ -42,7 +46,7 @@ namespace ClassLibrary1
                         else
                             Category[currentWindowTitle].StartTime = DateTime.Now;
                     }
-                    
+
                 }
 
             }
@@ -59,6 +63,7 @@ namespace ClassLibrary1
             else
                 coll[nameProcess][currentWindowTitle].StartTime = DateTime.Now;
 
+            StorageHasUpdated(this, new ProcessInfoEventArgs(currentWindowTitle, nameProcess, new TimeSpan(0, 0, interval)));
 
         }
         public void FinishTrack()
@@ -68,7 +73,7 @@ namespace ClassLibrary1
                 timer.Dispose();
                 timer = null;
             }
-            if (coll!= null)
+            if (coll != null)
             {
                 coll.Dispose();
                 coll = null;
--- ClassLibrary1/Interfaces.cs
+++ ClassLibrary1/Interfaces.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
+using System.Threading;
 namespace ClassLibrary1
 {
     interface IProcessInfo
@@ -30,7 +31,7 @@ namespace ClassLibrary1
     interface IProcessInfoGenerator
     {
         //public Process GetActiveWindowAsProcess();
-        //можно, но есть привязка к реализации через класс System.Diagnostic.Process
+        //можно, но есть привязка к реализации через класс System.Diagnostics.Process
         // мне больше нравится получать конкретные данные
         public int GetCurrentProcessId();
         public string GetCurrentProcessName();
@@ -38,8 +39,26 @@ namespace ClassLibrary1
         public string GetCurrentProcessTitle();
         public bool GetStartTimeOfCurrentProcess(out DateTime dateTime );
     }
+    class ProcessInfoEventArgs:EventArgs
+    {
+        private readonly string windowTitle, processName;
+        private readonly TimeSpan duration;
+        public ProcessInfoEventArgs(string windowTitle, string processName, TimeSpan duration)
+        {
+            this.windowTitle = windowTitle;
+            this.processName = processName;
+            this.duration = duration;
+        }
+        public string WindowTitle { get { return windowTitle; } }
+        public string ProcessName { get { return processName; } }
+        public TimeSpan Duration { get { return duration; } }
+
+    }
     interface IProcessInfoHandler<T>// как ограничить Т на ICategory && IRecord?
     {
+        public delegate void ProcessInfoEventHandler(Object sender, ProcessInfoEventArgs e);
+        public event ProcessInfoEventHandler StorageHasUpdated;
+
         public void StartTrack(IProcessInfoGenerator generator, IProcessStorage<T> collection, int intervals);
         public void FinishTrack();
     }
