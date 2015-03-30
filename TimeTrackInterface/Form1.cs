3ffe51b93da2575c94ef06bc21400696e1b862d7
fanatt20 <fanatt20@gmail.com>
2015-03-30 16:18:26 +0300

    Remove Excel export

--- TimeTrackInterface/Form1.cs
+++ TimeTrackInterface/Form1.cs
@@ -111,7 +111,7 @@ namespace WinFormsInterface
             if (saveFileDialog.FileName != "")
             {
                 try
-                {
+                { 
                     switch (saveFileDialog.FilterIndex)
                     {
                         case 1:
